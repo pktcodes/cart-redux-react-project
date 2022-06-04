fe6a31c59b694a3ec858f42edaa80e8d148d3231
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-04 11:20:35 -0500

    Actions as Variables

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 import { legacy_createStore as createStore } from "redux";
+import { DECREASE, INCREASE } from "./actions";
 
 //initial store
 const initialStore = {
-  count: 50,
+  count: 0,
   name: "praveen",
 };
 
@@ -17,31 +18,23 @@ const initialStore = {
 function reducer(state, action) {
   console.log({ state, action });
 
-  if (action.type === "DECREASE") {
+  if (action.type === DECREASE) {
     return { ...state, count: state.count - 1 };
   }
 
-  if (action.type === "INCREASE") {
+  if (action.type === INCREASE) {
     return { ...state, count: state.count + 1 };
   }
 
-  if (action.type === "RESET") {
-    return { ...state, count: 0 };
-  }
-
-  if (action.type === "CHANGE_NAME") {
-    return { ...state, name: "PK" };
-  }
   return state;
 }
 
 const store = createStore(reducer, initialStore);
 
 //getState - gets the current state
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "CHANGE_NAME" });
-store.dispatch({ type: "INCREASE" });
-store.dispatch({ type: "RESET" });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
 
 console.log(store.getState());
 
---
+++ src/actions.js
@@ -0,0 +1,4 @@
+export const DECREASE = "DECREASE";
+export const INCREASE = "INCREASE";
+export const REMOVE = "REMOVE";
+export const CLEAR_CART = "CLEAR_CART";
