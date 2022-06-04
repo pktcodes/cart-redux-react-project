c0ae486a74d22379c1be3352684666176377fe85
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-04 18:42:29 -0500

    Reducer Setup

--- src/reducer.js
+++ src/reducer.js
@@ -1,27 +1,31 @@
-import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
+import { DECREASE, INCREASE, REMOVE, CLEAR_CART } from "./actions";
 
 function reducer(state, action) {
   if (action.type === DECREASE) {
-    return { ...state, count: state.count - 1 };
+    console.log("You have decreased the amount");
   }
 
   if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 };
+    console.log("You have increased the amount");
+  }
+
+  if (action.type === REMOVE) {
+    console.log("You have removed the item");
   }
 
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
   }
   return state;
-
-  //   ================Switch Statement===============
-  //   switch (action.type) {
-  //     case CLEAR_CART:
-  //       return { ...state, cart: [] };
-  //     default:
-  //       return state;
-  //   }
-  //   ===============================================
 }
 
 export default reducer;
+
+//   ================Switch Statement===============
+//   switch (action.type) {
+//     case CLEAR_CART:
+//       return { ...state, cart: [] };
+//     default:
+//       return state;
+//   }
+//   ===============================================
