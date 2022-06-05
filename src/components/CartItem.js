476473372670f6bc4e46b3ba76e4deceba629369
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-04 22:40:03 -0500

    Increase and Decrease Setup

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,13 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import { DECREASE, INCREASE, REMOVE } from "../actions";
 
-const CartItem = ({ img, title, price, amount, remove }) => {
+const CartItem = ({
+  img,
+  title,
+  price,
+  amount,
+  remove,
+  increase,
+  decrease,
+}) => {
   return (
     <div className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
+
         {/* remove button */}
         <button
           className="remove-btn"
@@ -19,17 +28,30 @@ const CartItem = ({ img, title, price, amount, remove }) => {
           remove
         </button>
       </div>
+
       <div>
         {/* increase amount */}
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            increase();
+          }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
           </svg>
         </button>
+
         {/* amount */}
         <p className="amount">{amount}</p>
+
         {/* decrease amount */}
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            decrease();
+          }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
           </svg>
@@ -40,9 +62,12 @@ const CartItem = ({ img, title, price, amount, remove }) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  const { id } = ownProps;
+  const { id, amount } = ownProps;
   return {
     remove: () => dispatch({ type: REMOVE, payload: { id: id } }),
+    increase: () => dispatch({ type: INCREASE, payload: { id: id } }),
+    decrease: () =>
+      dispatch({ type: DECREASE, payload: { id: id, amount: amount } }),
   };
 };
 
